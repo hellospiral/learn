e6bab7438e577ff4c05e7caf84e9e0916295b0bc
Leah Nelson and Matt Carlson <leah+matt@>
2016-09-07 10:53:15 -0700

    add edit and delete for sections; fix edit/delete for lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -42,7 +42,7 @@ class LessonsController < ApplicationController
   def destroy
     @lesson = Lesson.find(params[:id])
     @lesson.destroy
-    redirect_to lessons_path
+    redirect_to section_lessons_path(@lesson.section)
   end
 
   private
