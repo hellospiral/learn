a049070bd545985fdf0fb3825bf6ce3837b4b444
Leah Nelson and Matt Carlson <matt@>
2016-09-07 09:09:17 -0700

    set up project, add lesson CRUD, prev and next lesson nav

---
+++ app/controllers/lessons_controller.rb
@@ -0,0 +1,47 @@
+class LessonsController < ApplicationController
+  def index
+    @lessons = Lesson.all
+  end
+
+  def show
+    @lessons = Lesson.all
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to lessons_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to lessons_path
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:name, :content, :number)
+  end
+end
